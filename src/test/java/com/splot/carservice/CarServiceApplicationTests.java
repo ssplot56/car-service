ab373e938fb4621a528f38a068db99950654c1e6
ssplot56 <ssplot56@gmail.com>
2023-02-08 20:45:30 +0200

    initial commit

---
+++ src/test/java/com/splot/carservice/CarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splot.carservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
